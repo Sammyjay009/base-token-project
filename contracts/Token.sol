25917cf83ad297c0c78dbafb24fc34559351dcb2
Sammyjay009 <jaiyeolasamuel3@gmail.com>
2026-03-26 08:01:30 +0000

    feat: inherit ERC20 in Token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -12,3 +12,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Token {
 
 }
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor() ERC20("SammyToken", "SMT") {}
+}
