dd5db0fa9fb0d150d3f65d542c5e9e85eb01933c
Sammyjay009 <jaiyeolasamuel3@gmail.com>
2026-03-26 08:03:07 +0000

    feat: add mint function stub

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -20,3 +20,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Token is ERC20 {
     constructor() ERC20("SammyToken", "SMT") {}
 }
+function mint(address to, uint256 amount) public {
+    // coming soon
+}
