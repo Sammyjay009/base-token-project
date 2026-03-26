d4ab72a48e4e261457db0282781a3aa955121f85
Sammyjay009 <jaiyeolasamuel3@gmail.com>
2026-03-26 16:32:14 +0000

    feat: add Minted event

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+event Minted(address indexed to, uint256 amount);
+function mint(address to, uint256 amount) public onlyOwner {
+    _mint(to, amount);
+    emit Minted(to, amount);
+}
+
 contract Token {
 
 }
