1cb9529076fd55f09aa15348d62ff1de7ca10715
Sammyjay009 <jaiyeolasamuel3@gmail.com>
2026-03-26 07:39:45 +0000

    chore: add Base network to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.20",
+  networks: {
+    base_sepolia: {
+      url: "https://sepolia.base.org",
+      chainId: 84532,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
