19e961d847fb4b4c399c95f9b0950104965416bb
Sammyjay009 <jaiyeolasamuel3@gmail.com>
2026-03-26 08:10:55 +0000

    feat: add owner variable

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -18,8 +18,13 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor() ERC20("SammyToken", "SMT") {}
-}
-function mint(address to, uint256 amount) public {
-    _mint(to, amount);
+    address public owner;
+
+    constructor() ERC20("SammyToken", "SMT") {
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
 }
