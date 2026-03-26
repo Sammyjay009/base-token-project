e6112499bb21560df5d59279f196a95bcdd85835
Sammyjay009 <jaiyeolasamuel3@gmail.com>
2026-03-26 17:49:52 +0000

    Update Token.test.js

--- contracts/Token.test.js
+++ contracts/Token.test.js
@@ -11,3 +11,9 @@ describe("Token", function () {
   return { token, owner, user };
 }
 });
+
+it("should mint tokens to owner", async function () {
+  const { token, owner } = await loadFixture(deployToken);
+  await token.mint(owner.address, 100);
+  expect(await token.balanceOf(owner.address)).to.equal(100);
+});
