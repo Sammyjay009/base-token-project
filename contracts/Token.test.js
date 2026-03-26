6affb6db37fcf7af955b60e161fd994cf2a36c87
Sammyjay009 <jaiyeolasamuel3@gmail.com>
2026-03-26 17:46:56 +0000

    Update Token.test.js

---
+++ contracts/Token.test.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+
+describe("Token", function () {
+  // tests coming
+});
