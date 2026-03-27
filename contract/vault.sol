cf08b2edee854bf2dd9a672b2911243ea29e9915
Sammyjay009 <jaiyeolasamuel3@gmail.com>
2026-03-27 13:13:39 +0000

    Update vault.sol

--- contract/vault.sol
+++ contract/vault.sol
@@ -4,3 +4,17 @@ pragma solidity ^0.8.20;
 contract Vault {
 
 }
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Vault {
+    IERC20 public token;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    function deposit(uint256 amount) public {
+        token.transferFrom(msg.sender, address(this), amount);
+    }
+}
