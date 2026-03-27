86cb0f09a0363034665af757f23d50571a719c2a
Sammyjay009 <jaiyeolasamuel3@gmail.com>
2026-03-27 13:41:12 +0000

    vault

--- contract/vault.sol
+++ contract/vault.sol
@@ -25,8 +25,7 @@ contract Vault {
 /// @notice Withdraws tokens from the vault
 /// @param amount Number of tokens to withdraw
 function withdraw(uint256 amount) public {
-    require(balances[msg.sender] >= amount, "Insufficient balance");
-    balances[msg.sender] -= amount;
+    require(balances[msg.sender] >= amount, "Vault: amount exceeds deposited balance");
     token.transfer(msg.sender, amount);
 }
 
